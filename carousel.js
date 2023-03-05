be8c053774bc62b7ee0cec7388590756344c700a
Sukrit Sethi <sukritsethi18@gmail.com>
2023-03-06 03:37:24 +0530

    button functionality

--- carousel.js
+++ carousel.js
@@ -1,4 +1,9 @@
 const myButton = document.getElementById("myButton");
+const textContainer = document.getElementById("textContainer");
+const myCarousel = document.getElementById("carousel");
+
 myButton.addEventListener("click", function() {
-  // Code to change the HTML document goes here
+    textContainer.classList.add("invisible");
+    myCarousel.classList.remove("invisible");
+    console.log("kuhyhvb");
 });
